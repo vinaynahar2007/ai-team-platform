ba37ad93e983f8907d3e5d699188e193387893a8
Vinay Kumar Nahar <vinaynahar2007@gmail.com>
2026-05-26 03:30:52 +0530

    main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
 from app.routes.auth import router as auth_router
+
 from app.database import engine, Base
+
 from app.models.user import User
 
 Base.metadata.create_all(bind=engine)
@@ -12,7 +15,7 @@ app.add_middleware(
     CORSMiddleware,
 
     allow_origins=[
-        "http://localhost:3000"
+        "*"
     ],
 
     allow_credentials=True,
