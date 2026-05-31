0c65712bcd0c4a7492e0f3605f5a13eeb77a7895
Vinay Kumar Nahar <vinaynahar2007@gmail.com>
2026-06-01 04:09:33 +0530

    Production ready setup

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-    "https://ai-team-platform-delta.vercel.app"
+    "https://ai-team-platform-delta.vercel.app",
+    "https://ai-team-platform-c3t9920gq-vinaynahar2007-2731s-projects.vercel.app"
     ],
     allow_credentials=False,
     allow_methods=["*"],
